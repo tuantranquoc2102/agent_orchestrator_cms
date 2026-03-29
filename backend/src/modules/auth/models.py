0dc43fd322bf5c82c8404de7dc46845d74510fb6
tuantranquoc2102 <143320458+tuantranquoc2102@users.noreply.github.com>
2026-03-29 22:00:40 +0700

    Initial commit backend/src/modules/auth/models.py

---
+++ backend/src/modules/auth/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+from sqlalchemy.sql import func
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
