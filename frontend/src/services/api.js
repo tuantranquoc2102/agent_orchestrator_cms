f4ccd057a4590d582aa594f0cdd825981b1e74e0
tuantranquoc2102 <143320458+tuantranquoc2102@users.noreply.github.com>
2026-04-01 14:40:47 +0700

    Initial commit frontend/src/services/api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error.response.data);
+  }
+);
+
+export default api;
